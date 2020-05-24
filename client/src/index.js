68bd20e9fd26a7107cb676b9aabbe943d72f52dc
imarg3 <imarg3@zoho.com>
2020-05-24 16:03:04 +0530

    React frontend with basic landing page

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/* React StrictMode is a feature added in version 16.3 and aimed to help us 
+ in finding potential problems in an application. Like Fragment, StrictMode does not render any UI, it only activates checks and adds warnings at runtime.
+ */
 ReactDOM.render(
   <React.StrictMode>
     <App />
