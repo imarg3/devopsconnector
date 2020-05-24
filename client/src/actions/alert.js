65a8cfca4caad28637577f26fe129d426d26db27
imarg3 <imarg3@zoho.com>
2020-05-24 16:13:22 +0530

    Fix uuid import

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'; // fixed uuid export default error
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
