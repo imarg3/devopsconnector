5efd5ba06a264a7334da09636b1422d7c4d6bb81
imarg3 <imarg3@zoho.com>
2020-05-24 14:09:12 +0530

    Auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,18 +5,23 @@ module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
-  // Check if no token
+  // Check if not token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   // Verify Token
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
-
-    req.user = decoded.user;
-    next();
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
   } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
   }
 };
