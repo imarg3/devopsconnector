ebd4297610d115a8221e4f8b307b8a1cfe0ff6c1
Arpit Gupta <imarg_3@zoho.com>
2020-06-25 17:48:52 +0530

    remove unused variables

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-// import Spinner from '../layout/Spinner';
-import { getGithubRepos } from '../../actions/profile';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getGithubRepos } from "../../actions/profile";
 
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   useEffect(() => {
@@ -10,13 +9,13 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   }, [getGithubRepos, username]);
 
   return (
-    <div className='profile-github'>
-      <h2 className='text-primary my-1'>Github Repos</h2>
+    <div className="profile-github">
+      <h2 className="text-primary my-1">Github Repos</h2>
       {repos.map((repo) => (
-        <div key={repo.id} className='repo bg-white p-1 my-1'>
+        <div key={repo.id} className="repo bg-white p-1 my-1">
           <div>
             <h4>
-              <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>
+              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
                 {repo.name}
               </a>
             </h4>
@@ -24,13 +23,13 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
           </div>
           <div>
             <ul>
-              <li className='badge badge-primary'>
+              <li className="badge badge-primary">
                 Stars: {repo.stargazers_count}
               </li>
-              <li className='badge badge-dark'>
+              <li className="badge badge-dark">
                 Watchers: {repo.watchers_count}
               </li>
-              <li className='badge badge-light'>Forks: {repo.forks_count}</li>
+              <li className="badge badge-light">Forks: {repo.forks_count}</li>
             </ul>
           </div>
         </div>
